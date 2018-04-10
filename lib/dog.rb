21d7940a6b48f9ada25d3fd16a171358e8855a0c
Eric Chrobak <eric@ericchrobak.com>
2018-04-10 00:31:43 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,14 @@
 class Dog
-  def name=(dog)
-    name=dog
+  def name=(name)
+    name=name
   end
   def name
     name
+  end
+  def breed=(breed)
+    breed=breed
+  end
+  def breed
+    breed
   end
 end
